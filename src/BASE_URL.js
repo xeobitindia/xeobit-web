5a563e07331f444e1a14e10adae72fba7d4fe271
Arnav Tiwari <arnav.luck@gmail.com>
2026-09-25 17:23:57 +0530

    mail_url change

--- src/BASE_URL.js
+++ src/BASE_URL.js
@@ -1,4 +1,4 @@
 const URL = "https://ftlop.xyz/xeobits/XEOBIT-win32-x64.zip"
-const MAIL_URL = "http://localhost:8000/api/contact"
+const MAIL_URL = "http://168.144.144.130:8000/api/contact"
 
 export default {URL, MAIL_URL};
